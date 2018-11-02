38256dee47e944f1a02e2e510961bad7554032a3
liupeng <LIUPENG.0@outlook.com>
2018-11-02 21:32:25 +0800

    小工具: 双钥制密钥 V 1.0.1

---
+++ src/Tools/Password/AddCoreValues.java
@@ -0,0 +1,68 @@
+package Tools.Password;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddCoreValues {
+
+    public AddCoreValues() {
+        init();
+        reInit();
+    }
+
+    private String password = null;
+
+    private Map <String, String> values = null;
+
+    private Map <String, String> reValues = null;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void init() {
+        values = new HashMap<String, String>();
+        String[] strings = {"平等", "公正", "文明", "和谐", "自由", "爱国", "敬业", "诚信", "法治", "富强", "社会主义核心价值观", "民主", "友善"};
+        for (int i=0;i<=9;i++) {
+            values.put(i+"", strings[i]);
+            //System.out.println(i);
+        }
+        values.put("u", strings[10]);
+        values.put("c", strings[11]);
+        values.put("e", strings[12]);
+
+        //System.out.println(values.get("2"));
+    }
+
+    public void reInit() {
+        reValues = new HashMap<String, String>();
+
+        String[] strings = {"平等", "公正", "文明", "和谐", "自由", "爱国", "敬业", "诚信", "法治", "富强", "社会主义核心价值观", "民主", "友善"};
+        for (int i=0;i<=9;i++) {
+            reValues.put(strings[i], i + "");
+        }
+        reValues.put(strings[10], "u");
+        reValues.put(strings[11], "c");
+        reValues.put(strings[12], "e");
+
+    }
+
+    public String getReValue(String key) {
+        //System.out.println(key);
+        String value = (String) this.reValues.get(key);
+        return value;
+    }
+
+    public String getValue(String key) {
+        String value = (String) this.values.get(key);
+        return value;
+    }
+
+    public static void main(String[] args) {
+        new AddCoreValues();
+    }
+}
---
+++ src/Tools/Password/doing.java
@@ -0,0 +1,39 @@
+package Tools.Password;
+
+public class doing {
+    public static void main(String[] args) {
+        String user = "liupeng.0@outlook.com";
+        String code = "核心价值观";
+
+        System.out.println("用户：" + user);
+
+        System.out.println("明文：" + code);
+
+        String password = new password(user).getPrivatePassword();
+        System.out.println("私钥：" + password);
+
+        DIYCode diyCode = new DIYCode();
+
+        String unicode = diyCode.unicode(code);
+        System.out.println("转化为unicode：" + unicode);
+
+        String myUnicode = diyCode.unicodeForMe(unicode);
+        System.out.println("我的unicode：" + myUnicode);
+
+        String yourUnicode = new CreateValues(password, myUnicode).getCoreValues();
+        System.out.println("与密钥结合：" + yourUnicode);
+
+        String valuees = new Encryption(yourUnicode).getText();
+        System.out.println("你的密文：" + valuees);
+
+        System.out.println("我的unicode：" + myUnicode);
+        myUnicode = new GetYourUnicode(valuees).getMyUnicode();
+        System.out.println("我的unicode：" + myUnicode);
+
+        String unicodeII = new DIYCode().reUnicodeForMe(myUnicode);
+        System.out.println("转化为unicode："+ unicodeII);
+
+        String string = new DIYCode().decodeUnicode(unicodeII);
+        System.out.println("明文：" + string);
+    }
+}
