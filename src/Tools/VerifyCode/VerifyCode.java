5a3f0a17f5887766eb76bc9ab75ae13057c827fa
liupeng <LIUPENG.0@outlook.com>
2018-11-02 21:30:30 +0800

    小工具: 验证码 V 1.0.1

---
+++ src/Tools/VerifyCode/VerifyCode.java
@@ -0,0 +1,112 @@
+package Tools.VerifyCode;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Random;
+
+public class VerifyCode {
+    private int w = 70;
+    private int h = 35;
+    private Random r = new Random();
+    // {"宋体", "华文楷体", "黑体", "华文新魏", "华文隶书", "微软雅黑", "楷体_GB2312"}
+    private String[] fontNames  = {"宋体", "华文楷体", "黑体", "微软雅黑", "楷体_GB2312"};
+    // 可选字符
+    private String codes  = "0123456789abcdefghjkmnopqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ";
+    // 背景色
+    private Color bgColor  = new Color(188, 248, 255);
+    // 验证码上的文本
+    private String text ;
+
+    // 生成随机的颜色
+    private Color randomColor () {
+        int red = r.nextInt(150);
+        int green = r.nextInt(150);
+        int blue = r.nextInt(150);
+        return new Color(red, green, blue);
+    }
+
+    // 生成随机的字体
+    private Font randomFont () {
+        int index = r.nextInt(fontNames.length);
+        String fontName = fontNames[index];//生成随机的字体名称
+        int style = r.nextInt(4);//生成随机的样式, 0(无样式), 1(粗体), 2(斜体), 3(粗体+斜体)
+        int size = r.nextInt(5) + 24; //生成随机字号, 24 ~ 28
+        return new Font(fontName, style, size);
+    }
+
+    // 画干扰线
+    private void drawLine (BufferedImage image) {
+        int num  = 3;//一共画3条
+        Graphics2D g2 = (Graphics2D)image.getGraphics();
+        for(int i = 0; i < num; i++) {
+            //生成两个点的坐标，即4个值
+            int x1 = r.nextInt(w);
+            int y1 = r.nextInt(h);
+
+            int x2 = r.nextInt(w);
+            int y2 = r.nextInt(h);
+
+            g2.setStroke(new BasicStroke(1.5F));
+            g2.setColor(Color.BLUE); //干扰线是蓝色
+            g2.drawLine(x1, y1, x2, y2);//画线
+        }
+    }
+
+    // 随机生成一个字符
+    private char randomChar () {
+        int index = r.nextInt(codes.length());
+        return codes.charAt(index);
+    }
+
+    // 创建BufferedImage
+    private BufferedImage createImage () {
+        // 构造的 BufferedImage预定义的图像类型。
+        BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2 = (Graphics2D)image.getGraphics();
+        g2.setColor(this.bgColor);
+        g2.fillRect(0, 0, w, h);
+        return image;
+    }
+
+    // 调用这个方法得到验证码
+    public BufferedImage getImage () {
+        //创建图片缓冲区
+        BufferedImage image = createImage();
+        //得到绘制环境
+        Graphics2D g2 = (Graphics2D)image.getGraphics();
+        //用来装载生成的验证码文本
+        StringBuilder stringBuilder = new StringBuilder();
+        // 向图片中画4个字符
+        //循环四次，每次生成一个字符
+        for(int i = 0; i < 4; i++)  {
+            //随机生成一个字母
+            String string = randomChar() + "";
+            //把字母添加到sb中
+            stringBuilder.append(string);
+            //设置当前字符的x轴坐标
+            float x = i * 1.0F * w / 4;
+            //设置随机字体
+            g2.setFont(randomFont());
+            //设置随机颜色
+            g2.setColor(randomColor());
+            g2.drawString(string, x, h-5); //画图
+        }
+        this.text = stringBuilder.toString(); //把生成的字符串赋给了this.text
+        drawLine(image); //添加干扰线
+        return image;
+    }
+
+    // 返回验证码图片上的文本
+    public String getText () {
+        return text;
+    }
+
+    // 保存图片到指定的输出流
+    public static void output (BufferedImage image, OutputStream out)
+            throws IOException {
+        ImageIO.write(image, "JPEG", out);
+    }
+}
--- web/index.jsp
+++ web/index.jsp
@@ -8,7 +8,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>加密交友网（beat）</title>
   </head>
   <body>
   $END$
