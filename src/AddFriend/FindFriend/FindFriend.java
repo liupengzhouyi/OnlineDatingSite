722d5f2e5fdd706a22ff7058e521795e13f6c2f9
liupeng <LIUPENG.0@outlook.com>
2018-11-05 15:39:24 +0800

    添加好友 V1.0.4

--- src/AddFriend/FindFriend/FindFriend.java
+++ src/AddFriend/FindFriend/FindFriend.java
@@ -2,6 +2,16 @@ package AddFriend.FindFriend;
 
 public class FindFriend {
 
+    public FindFriend(String friendId) {
+        init(friendId);
+
+
+    }
+
+    public void init(String friendId) {
+        this.setFriendId(friendId);
+    }
+
     public String getFriendId() {
         return friendId;
     }
