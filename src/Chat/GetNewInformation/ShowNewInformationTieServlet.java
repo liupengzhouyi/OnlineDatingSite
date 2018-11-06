0345804b5f9c86917eb7f04bcad183b3a9e853e9
liupeng <LIUPENG.0@outlook.com>
2018-11-06 12:22:54 +0800

    聊天.获取新信息提示(1) V1.0.5

---
+++ src/Chat/GetNewInformation/ShowNewInformationTieServlet.java
@@ -0,0 +1,85 @@
+package Chat.GetNewInformation;
+
+import Chat.FindNewInformation.HaveNewInformation;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+@WebServlet(name = "ShowNewInformationTieServlet")
+public class ShowNewInformationTieServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //设置编码格式
+        //response.setContentType("");
+        //初始化函数
+        this.init(request);
+        //获取PrintWirter对象
+        PrintWriter printWriter = response.getWriter();
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    /**
+     * 初始化函数
+     * @param request
+     */
+    public void init(HttpServletRequest request) {
+        //获取Session对象
+        HttpSession httpSession = request.getSession();
+        //获取和设置用户的用户的账号
+        this.setUser_id((String) httpSession.getAttribute("user_id"));
+
+        try {
+            //获取 查看新信息工具类 对象
+            HaveNewInformation haveNewInformation = new HaveNewInformation(this.getUser_id());
+            //获取设置 新信息数量
+            this.setNumber(haveNewInformation.getNumber());
+            //获取设置 新信息编号
+            this.setTies(haveNewInformation.getTies());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String[] getTies() {
+        return ties;
+    }
+
+    public void setTies(String[] ties) {
+        this.ties = ties;
+    }
+
+    private String[] ties ;
+
+    private int number = -1;
+
+    private String user_id = "";
+}
