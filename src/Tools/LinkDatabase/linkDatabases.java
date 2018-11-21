b84dfba9c83755a38b80a7b1050187fe3fea47f3
liupeng <LIUPENG.0@outlook.com>
2018-11-21 10:36:53 +0800

    MAC修改数据库 V1.0.0

--- src/Tools/LinkDatabase/linkDatabases.java
+++ src/Tools/LinkDatabase/linkDatabases.java
@@ -8,7 +8,7 @@ public class linkDatabases {
     //Window
     //static final String DB_URL = "jdbc:mysql://localhost:3306/database_for_java_web";
     //Mac
-    static final String DB_URL = "jdbc:mysql://localhost:3306/make_friend_by_java";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/online_dating_site";
     // 数据库的用户名与密码，需要根据自己的设置
     static final String USERNAME = "root";
     static final String PASSWORD = "lp184126";
